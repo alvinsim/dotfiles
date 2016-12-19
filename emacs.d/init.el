6e05613dbdf1ec0fe0fcf0d26c8e2c20608bf202
Alvin Sim <alvinsim@freebsd.alvinsim.com>
2016-12-19 17:49:04 +0800

    cleaned-up formatting and removed some unrelated information

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,24 +1,24 @@
-; Personal information
-(defun asim/personal-information()
+;; Personal information
+(defun asim/personal-information ()
   (setq user-full-name "Alvin Sim"
 	user-mail-address "sim.alvin@gmail.com"))
 
 ;; packages
-(defun asim/packages()
-  (setq asim-packages '(company
-												beacon
-												leuven
-												projectile
-												flx-ido
-												magit
-												atom-one-dark-theme
-												yasnippet
-												clojure
-												cider
-												solarized-theme
-												dash
-												paredit
-												powerline))
+(defun asim/packages ()
+  (setq asim/use-packages '(company
+			    beacon
+			    leuven
+			    projectile
+			    flx-ido
+			    magit
+			    atom-one-dark-theme
+			    yasnippet
+			    clojure
+			    cider
+			    solarized-theme
+			    dash
+			    paredit
+			    powerline))
 
   ;; Init packages and add package archives
   (load "package")
@@ -30,7 +30,7 @@
     (when (not package-archive-contents)
       (package-refresh-contents)
       (setq refresh t))
-    (dolist (pkg asim-packages)
+    (dolist (pkg asim/use-packages)
       (when (and (not (package-installed-p pkg))
 		 (assoc pkg package-archive-contents))
 	(unless refreshed
@@ -38,51 +38,51 @@
 	  (setq refreshed t))
 	(package-install pkg)))))
 
-(defun asim/indicate-empty-lines()
+(defun asim/indicate-empty-lines ()
   (setq-default indicate-empty-lines t)
   (when (not indicate-empty-lines)
-		(toggle-indicate-empty-lines)))
+    (toggle-indicate-empty-lines)))
 
 ;; windows configuration
-(defun asim/window-system()
+(defun asim/window-system ()
   (when window-system
-		(tooltip-mode -1)
-		(tool-bar-mode -1)
-		(menu-bar-mode 1)
-		(scroll-bar-mode -1)
-		;; theme
-		(load-theme 'atom-one-dark t))
+    (tooltip-mode -1)
+    (tool-bar-mode -1)
+    (menu-bar-mode 1)
+    (scroll-bar-mode -1)
+    ;; theme
+    (load-theme 'atom-one-dark t))
   (add-to-list 'write-file-functions 'delete-trailing-whitespace))
 
 ;; winner-mode
-(defun asim/winner-mode()
+(defun asim/winner-mode ()
   (when (fboundp 'winner-mode)
-		(winner-mode 1)))
+    (winner-mode 1)))
 
 ;; utf-8 encoding
-(defun asim/utf8-encoding()
+(defun asim/utf8-encoding ()
   (prefer-coding-system 'utf-8)
   (when (display-graphic-p)
-		(setq x-select-request-type '(UTF8-STRING_COMPOUND_TEXT TEXT STRING))))
+    (setq x-select-request-type '(UTF8-STRING_COMPOUND_TEXT TEXT STRING))))
 
 ;; paredit
-(defun asim/paredit()
-	(add-hook 'clojure-mode-hook 'paredit-mode)
-	(add-hook 'clojurescript-mode-hook 'paredit-mode))
+(defun asim/paredit ()
+  (add-hook 'clojure-mode-hook 'paredit-mode)
+  (add-hook 'clojurescript-mode-hook 'paredit-mode))
 
 ;; other misc configuration
 (defun asim/other ()
   (setq-default tab-width 2)
   (setq inhibit-splash-screen t
-		indent-tabs-mode t
-		line-number-mode 1
-		column-number-mode t
-		show-trailing-whitespace t
-		sentence-end-double-space nil
-		echo-keystrokes 0.1
-		use-dialog-box nil
-		visible-bell t
-		transient-mark-mode 1)
+	indent-tabs-mode t
+	line-number-mode 1
+	column-number-mode t
+	show-trailing-whitespace t
+	sentence-end-double-space nil
+	echo-keystrokes 0.1
+	use-dialog-box nil
+	visible-bell t
+	transient-mark-mode 1)
   (show-paren-mode t)
   (defalias 'yes-or-no-p 'y-or-n-p)	;; make all "yes or no" to "y or n"
   (global-visual-line-mode 1)		;; visual mode word wrap
@@ -93,19 +93,14 @@
 ;; ido
 (defun asim/ido ()
   (setq ido-enable-flex-matching t
-		ido-everywhere t)
+	ido-everywhere t)
   (ido-mode 1))
 
 ;; org mode
 (defun asim/org ()
-  (setq org-agenda-files (list "/Users/608481089/Work/AIB/aib.org"
-															 "/Users/608481089/Work/AIB/Avaya/avaya.org"
-															 "/Users/608481089/Work/AIB/UKB/ukb.org"))
+  (setq org-agenda-files (list "/path/to/file.org"))
   (setq org-todo-keywords
-		'((sequence "TODO(t)" "WAITING(w)" "DOING(g)" "|" "DONE(d)" "CANCEL(c)")
-		  (sequence "ISSUE(i)" "INVESTIGATE(v)" "|" "DONE(d)" "CANCEL(c)")
-		  (sequence "CFT" "DEVELOPMENT" "CIT" "E2E" "DR" "|" "LIVE" "DEFERRED")
-		  (sequence "RED(r)" "|" "GREEN(e)")))
+	'((sequence "TODO(t)" "WAITING(w)" "DOING(g)" "|" "DONE(d)" "CANCEL(c)")))
 
   ;; keybindings
   (global-set-key "\C-cl" 'org-store-link)
@@ -116,8 +111,8 @@
   (eval-after-load "org"  '(require 'ox-md nil t))
 
   (setq org-catch-invisible-edits 1
-		org-hide-emphasis-markers t
-		org-ellipsis " .."))
+	org-hide-emphasis-markers t
+	org-ellipsis " .."))
 
   ;; org bullets
   ;; (require 'org-bullets)
@@ -135,9 +130,9 @@
 (defun asim/multi-term ()
   (require 'multi-term)
   (setq multi-term-program "/usr/bin/bash"
-		term-unbind-key-list '("C-z" "C-x" "C-h" "C-y")
-		multi-term-dedicated-select-after-open-p t
-		multi-term-dedicated-close-back-to-open-buffer-p t)
+	term-unbind-key-list '("C-z" "C-x" "C-h" "C-y")
+	multi-term-dedicated-select-after-open-p t
+	multi-term-dedicated-close-back-to-open-buffer-p t)
   (define-key global-map (kbd "C-x t") 'multi-term-dedicated-toggle))
 
 ;; projectile
@@ -154,7 +149,7 @@
 
   ;; disable ido faces to see flx highlights
   (setq ido-enable-flex-matching t
-		ido-use-faces nil))
+	ido-use-faces nil))
 
 ;; org-babel
 (defun asim/plantuml ()
@@ -164,13 +159,13 @@
 (defun asim/magit ()
   (require 'magit)
   (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key	(kbd "C-x M-g") 'magit-dispatch-popup)
+  (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
   (global-set-key (kbd "C-c M-g") 'magit-file-popup)
   (global-magit-file-mode))
 
 ;; cider
 (defun asim/cider ()
-	(setq cider-repl-pop-to-buffer-on-connect nil))
+  (setq cider-repl-pop-to-buffer-on-connect nil))
 
 ;;; key-bindings
 
