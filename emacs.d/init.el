efd671b520c0ea274043de814d5d83693c201504
Sim <alvin.sim@bt.com>
2017-12-11 13:28:48 +0000

    Corrected closing parenthesis

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -239,7 +239,7 @@
 
 	; py-autopep8
 	(require 'py-autopep8)
-	(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
+	(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
 
 ;; clojure cookbook
 (defun asim/clojure-cookbook ()
