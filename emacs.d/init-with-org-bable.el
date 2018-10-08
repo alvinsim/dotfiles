755c0842353c9b7c0fd35608c5d9c6417e00bcdc
alvinsim <sim.alvin@gmail.com>
2018-10-09 01:24:50 +1100

    fixed minor issue and also to install 'org' if the version of emacs doesn't come with org pre-installed

--- emacs.d/init-with-org-bable.el
+++ emacs.d/init-with-org-bable.el
@@ -1,3 +1,13 @@
+;; install org mode if for some reason this version of emacs doesn't
+;; have org-mode pre-installed
+(require 'package)
 (package-initialize)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-refresh-contents)
+
+(unless (package-installed-p 'org)
+	(package-install 'org))
+(unless (package-installed-p 'org-plus-contrib)
+	(package-install 'org-plus-contrib))
 
 (org-babel-load-file "~/.emacs.d/config.org")
