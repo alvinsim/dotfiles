d9c4593eca174aa8498f35dd25b2b8e5bd8793c9
Alvin Sim <sim.alvin@gmail.com>
2022-04-25 18:00:40 +1000

    Update config for org package changes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,12 +2,13 @@
 ;; have org-mode pre-installed
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-refresh-contents)
 
 (unless (package-installed-p 'org)
 	(package-install 'org))
-(unless (package-installed-p 'org-plus-contrib)
-	(package-install 'org-plus-contrib))
+(unless (package-installed-p 'org-contrib)
+	(package-install 'org-contrib))
 
 (org-babel-load-file "~/.emacs.d/config.org")
